88f5391e00cfce3e3c710771bf1ed91b9fc3fa51
Liam Deacon <liam.m.deacon@gmail.com>
2015-11-20 18:08:09 +0000

    Added Doxygen markup and minor refactoring

--- src/rfac/caoi_rfac_ctr.c
+++ src/rfac/caoi_rfac_ctr.c
@@ -1,22 +1,21 @@
 /************************************************************************
  *                        CAOI_RFAC_CTR.C
  *
- *  Copyright 2008-2014 Zhasmina Zheleeva
- *  Copyright 2014 Liam Deacon <liam.m.deacon@gmail.com>
+ *  Copyright 2008-2015 Zhasmina Zheleeva
+ *  Copyright 2014-2015 Liam Deacon <liam.m.deacon@gmail.com>
  *
  *  Licensed under GNU General Public License 3.0 or later.
  *  Some rights reserved. See COPYING, AUTHORS.
  *
  * @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
  *
- * Description:  file contains functions
- *  ctrinp()
- *    Makes .ctr file for each angle of incidence
- *
- * Changes:
- *   ZZ/2008.03.03 - creation
- *   LD/18.09.2014 - changed lengthctr, ictr & num to size_t from int
  *********************************************************************/
+
+/*!
+ * \file
+ *
+ * \author Zhasmina Zheleeva
+ */
 
 #include "rfac.h"
 #include <stdio.h>
@@ -25,9 +24,16 @@
 #include <string.h>
 #include <errno.h>
 
-int rfac_create_control_file_duplicates(const char *control_filename, size_t n)
+/*!
+ * Spawns @p n_ctr copies of the file given by @p control_filename and appends
+ * the number followed by '.ctr' e.g. <control_filename>_<i>.ctr
+ */
+int rfac_create_control_file_duplicates(
+    const char *control_filename,
+    size_t n_ctr)
 {
   char fname_ctr[FILENAME_MAX] = "";
+  char fname_workstr[FILENAME_MAX] = ""
   char string_ctr[STRSZ] = "";
   char workstr[STRSZ] = "";
 
@@ -35,7 +41,7 @@ int rfac_create_control_file_duplicates(const char *control_filename, size_t n)
   FILE *fp = NULL;
   const size_t length_max = sizeof(string_ctr)-1;
   size_t length_ctr = 0;
-  size_t ictr = 0;
+  size_t i_ctr = 0;
   size_t num = 0;
 
   strncpy(string_ctr, control_filename, length_max);
@@ -54,21 +60,21 @@ int rfac_create_control_file_duplicates(const char *control_filename, size_t n)
    For N-th  angle pair makes ....ia_N.ctr
    ******************************************************************************/
 
-  for (ictr = 0; ictr < n; ictr++)
+  for (i_ctr = 0; i_ctr < n_ctr; i_ctr++)
   {
     rewind(fp);
     strncpy(fname_ctr, control_filename, sizeof(fname_ctr)-1);
     fname_ctr[sizeof(fname_ctr)-1] = '\0';
 
-    snprintf(workstr, sizeof(workstr), "ia_%u.ctr", ictr);
-    if (length_ctr + strlen(workstr) >= sizeof(fname_ctr))
+    snprintf(fname_workstr, sizeof(fname_workstr)-1, "ia_%u.ctr", i_ctr);
+    if (length_ctr + strlen(fname_workstr) >= sizeof(fname_ctr))
     {
       ERROR_MSG("control filename too long\n");
       exit(ENAMETOOLONG);
     }
     snprintf(fname_ctr+length_ctr,
              sizeof(fname_ctr)-length_ctr,
-             "ia_%u.ctr", ictr + 1);
+             "ia_%u.ctr", i_ctr + 1);
 
     if ((fpf = fopen(fname_ctr, "w")) == NULL)
     {
@@ -82,7 +88,7 @@ int rfac_create_control_file_duplicates(const char *control_filename, size_t n)
       {
         sscanf(string_ctr + 3, " %u ", &num);
 
-        if ((ictr + 1) == num)
+        if ((i_ctr + 1) == num)
         {
           sscanf(string_ctr + 3, " %u %s", &num, workstr);
           fprintf(fpf, "%s\n", workstr);
