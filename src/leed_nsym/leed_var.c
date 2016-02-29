ce4215543cc8da565c11b655c78029bbcae875a2
Liam Deacon <liam.m.deacon@gmail.com>
2016-02-29 18:55:37 +0000

    Added leed_var_init() function

--- src/leed_nsym/leed_var.c
+++ src/leed_nsym/leed_var.c
@@ -23,6 +23,45 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+/*!
+ * Allocates and initialises a #leed_var structure to heap and initialises its
+ * values.
+ *
+ * \param vr Real part of the optical potential
+ * \param vi Imaginary part of the optical potential
+ *
+ * \return Pointer to initialised #leed_var
+ *
+ * \warning The member \c p_tl is initialised to \c NULL .
+ */
+
+
+void leed_var_init(leed_var *var_par, const real vr, const real vi)
+{
+  size_t i_c;
+
+  /* preset values */
+  var_par->eng_r = 0.;
+  var_par->eng_i = 0.;
+  var_par->eng_v = 0.;
+
+  var_par->vi_pre = vi;
+  var_par->vi_exp = 0.;
+  var_par->vr     = vr;
+
+  for( i_c = 0; i_c <=3; i_c ++) var_par->k_in[i_c] = 0.;
+
+  var_par->p_tl = NULL;
+
+  /* to be read in this function: */
+  var_par->theta = var_par->phi = 0.;
+  var_par->epsilon = WAVE_TOLERANCE;
+  var_par->l_max = 0;
+}
+
+/*!
+ * Frees a #leed_var structure given by \p var from memory
+ */
 void leed_var_free(leed_var *var) {
   if(var->p_tl) matarrfree(var->p_tl);
   free(var);
