db86d88cf39cdc65fdf4e3757f6ff5c517f7ebef
Liam-Deacon <liam.m.deacon@gmail.com>
2026-01-09 13:02:05 +0000

    fix(rfac): restore modern rfac api

---
+++ src/rfac/rfrb.c
@@ -0,0 +1,76 @@
+/*********************************************************************
+ *                           RFRB.C
+ *
+ *  Copyright 1992-2014 Georg Held <g.held@reading.ac.uk>
+ *
+ *  Licensed under GNU General Public License 3.0 or later.
+ *  Some rights reserved. See COPYING, AUTHORS.
+ *
+ * @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+ *
+ * Changes:
+ *   GH/1993.02.19 - Creation
+ *   GH/1994.02.08  Change Normalistion (Cuncorr = 0.67)
+ *   GH/1995.09.07 -  Adaption for CRFAC
+ ********************************************************************/
+
+/*! \file
+ *
+ * Implemenation file for Rb1 factor calculation.
+ */
+
+#include <stdio.h>
+#include <math.h>           /* needed for sqrt */
+#include "rfac.h"           /* specific definitions etc. */
+
+static const double CNORM = 0.666667; /*!< 2/3 normalisation to uncorrelated curves */
+
+/*!
+ * \brief Calculate Rb1-factor.
+ *
+ * Calculate Rb1-factor:
+ *
+ * \f$ R_{b1} = \frac{(1 - C)}{(1 - C_NORM)} \f$
+ *
+ * \f$ C = \frac{S(I_t \times I_e)}{\sqrt{ (S I_t^2) \cdot (S I_e^2)}} \f$
+ * \f$ C_{NORM} = \frac{2}{3} \f$ (cf. G. Held, Dissertation)
+ *
+ * \param[in] eng pointer to list of energies.
+ * \param[in] e_int pointer to list of experimental intensities.
+ * \param[in] t_int lists pointer to list of theoretical intensities.
+ *
+ * \note \p eng must be terminated by #F_END_OF_LIST.
+ *
+ * \return R2-factor if successful.
+ * \retval #RFAC_FAILURE, if failed.
+ *
+ */
+real rfac_rb(const real *eng, const real *e_int, const real *t_int)
+{
+  size_t i_eng;
+  real exp_sq_sum, the_sq_sum;
+  real rfac_sum;
+
+  /* Calculate integrals and R-factor */
+  exp_sq_sum = 0.;
+  the_sq_sum = 0.;
+  rfac_sum = 0.;
+
+  for(i_eng = 0; ! IS_EQUAL_REAL(eng[i_eng], (real)F_END_OF_LIST); i_eng ++)
+  {
+    rfac_sum += e_int[i_eng] * t_int[i_eng];
+
+    exp_sq_sum += SQUARE(e_int[i_eng]);
+    the_sq_sum += SQUARE(t_int[i_eng]);
+  }
+
+  rfac_sum /= cleed_real_sqrt(the_sq_sum * exp_sq_sum);
+
+  CONTROL_MSG(CONTROL, "rfac_sum: %f,", rfac_sum);
+
+  rfac_sum = (real)( (1. - rfac_sum) / (1. - CNORM) );
+
+  CONTROL_MSG(CONTROL, "%f \n", rfac_sum);
+
+  return (rfac_sum);
+}
---
+++ src/rfac/rfrdexpt.c
@@ -0,0 +1,179 @@
+/*********************************************************************
+ *                       RFRDEXPT.C
+ *
+ *  Copyright 1992-2014 Georg Held
+ *
+ *  Licensed under GNU General Public License 3.0 or later.
+ *  Some rights reserved. See COPYING, AUTHORS.
+ *
+ * @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+ *
+ * Changes:
+ *   GH/1992.11.11 - creation.
+ ********************************************************************/
+
+/*! \file
+ *  \brief Implementation of rfac_iv_read() function.
+ */
+
+#include <math.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <malloc.h>
+
+#include "rfac.h"       /* rf specific definitions */
+
+/*!
+ * Read experimental IV curve from file specified by filename and check if
+ * energy values are equidistant and sorted.
+ *
+ * The order of the input file must be:
+ * <energy> <intensity>
+ * Comments are indicated by a '#'.
+ *
+ * \param[in] filename string containing the name of the file where the
+ * input should be read from.
+ *
+ * \return pointer to the IV curve (#rfac_iv). The IV list is terminated
+ * by a pair (energy, intensity) of negative values.
+ *
+ * \retval \c NULL if failed.
+ */
+rfac_iv *rfac_iv_read(const char *filename)
+{
+
+  size_t i;
+  size_t lines;
+  long offs, len;
+  real max_int;
+
+  char line_buffer[STRSZ];
+  char *buffer;
+  char fmt_buffer[STRSZ];
+  rfac_iv *iv = rfac_iv_init();
+
+  /*
+   * copy input file to buffer
+   * count lines  without comments (i.e. number of data pairs)
+   * allocate memory for expt. IV curve
+   */
+
+  sprintf(fmt_buffer, "%%%sf %%%sf", CLEED_REAL_FMT, CLEED_REAL_FMT);
+
+  CONTROL_MSG(CONTROL, "opening \"%s\"\n", filename);
+
+  if( (buffer = file2buffer(filename)) == NULL)
+  {
+    ERROR_MSG("error in function file2buffer \n");
+    return(NULL);
+  }
+  lines = rfac_nclines(buffer);
+
+  CONTROL_MSG(CONTROL_X, "file content:\n%s\n", buffer);
+  CONTROL_MSG(CONTROL_X, "lines = %d\n", lines);
+
+  if (iv->data != NULL) free(iv->data);
+  CLEED_ALLOC_CHECK(iv->data = (rfac_iv_data *) 
+                      calloc((lines+1), sizeof(rfac_iv_data)));
+
+  CONTROL_MSG(CONTROL_X, "after malloc\n");
+
+  /* write energy pairs to list */
+  for ( i = 0, offs = 0L, max_int = 0.,
+       iv->equidist = true , iv->sort = true ;
+      ((len = bgets(buffer, offs, STRSZ, line_buffer))+1 ) /* orig. len > -1 */
+      && (i < lines);  offs += len)
+  {
+    
+    if(line_buffer[0] != '#')      /* no comment */
+    {
+
+      /* print eV[i] I[i] */
+      CONTROL_MSG(CONTROL_X, "(%s): %e %e\n", filename,
+                  iv->data[i].energy, iv->data[i].intens);
+
+      if(sscanf(line_buffer, fmt_buffer, &iv->data[i].energy, &iv->data[i].intens) == 2)
+      {
+        /* search for max. intensity */
+        if(iv->data[i].intens > max_int) max_int = iv->data[i].intens;
+
+        /* check if list is sorted */
+        if( (i > 0) && (iv->data[i].energy < iv->data[i-1].energy) )
+        {
+          iv->sort = false;
+        }
+
+        /* check equidistance */
+        if( (i > 1) &&
+            (cleed_real_fabs ( (2*iv->data[i-1].energy - iv->data[i].energy -
+                       iv->data[i-2].energy)) >  ENG_TOLERANCE ) )
+        {
+          iv->equidist = 0;
+          WARNING_MSG("\"%s\" is not equidistant (No:%d)\n", filename, i-1);
+        }
+
+        /* increment i */
+        i++;
+
+      } /* if sscanf */
+
+    } /* if line_buffer[0] != '#' */
+
+  } /* for */
+
+  if (i==0)
+  {
+    WARNING_MSG("No input read from file \"%s\"\n", filename);
+    i++;
+  }
+  else
+  {
+    CONTROL_MSG(CONTROL, "last pair(%d): %f %f\n",
+                 i-1, iv->data[i-1].energy, iv->data[i-1].intens);
+  }
+
+  if(iv->equidist == true)
+  {
+    CONTROL_MSG(CONTROL, "expt. IV curve is equidistant\n");
+  }
+  else
+  {
+    CONTROL_MSG(CONTROL, "expt. IV curve is not equidistant\n");
+  }
+
+  if (!iv) 
+    goto error;
+  if (!iv->data)
+    goto error;
+
+  /* write available information to structure iv_cur. */
+  iv->first_eng = iv->data[0].energy;
+  iv->last_eng = iv->data[i-1].energy;
+  iv->max_int = max_int;
+
+  CONTROL_MSG(CONTROL, "iv_cur structure written\n");
+ 
+  /* set last energy and intensities to termination value */
+  iv->data[i].energy = (real) F_END_OF_LIST;  /* termination */
+  iv->data[i].intens = (real) F_END_OF_LIST;  /* termination */
+  iv->n_eng = i;
+
+  CONTROL_MSG(CONTROL, "list terminations set\n");
+
+  /*
+   free previously allocated memory;
+   return;
+  */
+  free(buffer);
+
+  return(iv);
+
+  /* execute error clean up code */
+error:
+  free(buffer);
+  rfac_iv_free(iv);
+  return NULL;
+
+}
